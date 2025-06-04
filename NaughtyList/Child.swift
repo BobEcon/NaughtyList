4f1cfea1f66188cb098da65a834d245fb03b7209
Robert Beachill <bbeachill@icloud.com>
2025-06-04 23:56:00 +0100

    Started the app. Created Model class. Added some mock data for preview (still to complete)

---
+++ NaughtyList/Child.swift
@@ -0,0 +1,63 @@
+//
+//  Child.swift
+//  NaughtyList
+//
+//  Created by Robert Beachill on 04/06/2025.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+@Model
+class Child {
+    var firstName = ""
+    var lastName = ""
+    var naughty = true
+    var smacks = 1
+    var notes = ""
+    
+    init(firstName: String = "", lastName: String = "", naughty: Bool = true, smacks: Int = 1, notes: String = "") {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.naughty = naughty
+        self.smacks = smacks
+        self.notes = notes
+    }
+}
+
+extension Child {
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: Child.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        container.mainContext.insert(
+            Child(
+                firstName: "Bad",
+                lastName: "Bunny",
+                naughty: true,
+                smacks: 1,
+                notes: "Conejo Malo"
+            )
+        )
+        container.mainContext.insert(
+            Child(
+                firstName: "Bad",
+                lastName: "Bunny",
+                naughty: true,
+                smacks: 1,
+                notes: "Conejo Malo"
+            )
+        )
+        container.mainContext.insert(
+            Child(
+                firstName: "Bad",
+                lastName: "Bunny",
+                naughty: true,
+                smacks: 1,
+                notes: "Conejo Malo"
+            )
+        )
+        
+        return container
+    }
+}
