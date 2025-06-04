19b89188c49b820eb1dfc2ccc526db4b22de79fa
Robert Beachill <bbeachill@icloud.com>
2025-06-04 22:56:55 +0100

    Initial Commit

---
+++ NaughtyList/NaughtyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaughtyListApp.swift
+//  NaughtyList
+//
+//  Created by Robert Beachill on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct NaughtyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
